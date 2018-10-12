9727230748c2a8587fa30b2364c705aa1b7f8657
Ranika Nisal <ranikamadurawe.16@cse.mrt.ac.lk>
2018-10-12 17:24:25 +0530

    update4

---
+++ dbOperations/addUser.php
@@ -0,0 +1,19 @@
+<?php
+  // get provided username and encrypted password
+  $provided_id = ( $_POST['pending_id'] );
+  echo $provided_id;
+
+
+
+
+  // Login to DB as a Representative
+  /*$connection = mysqli_connect("localhost", "root", "", "authentication");
+
+  // query
+  $result = mysqli_query($connection, $query);
+
+  mysqli_close($connection);
+
+  */
+  //header('Location: ../Rep/viewclients.php ');
+?>
