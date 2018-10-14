8bc65a386a383aadc9265baa530a6b696b79c694
sadeepdarshana <darshanasadeep@gmail.com>
2018-10-14 18:03:24 +0530

    Update loginfun.php

--- login/loginfun.php
+++ login/loginfun.php
@@ -2,9 +2,4 @@
 
 
 require_once '../Classes/client.php';
-require_once '../Classes/rep.php';
-require_once '../Classes/officer.php';
-require_once '../Classes/manager.php';
-require_once '../dbOperations/session.php';
-include('../dbOperations/config 2.php');
 ?>
